12cf7976921ad77b6d9a44d1a3953d902f10efee
Xenia <ixel+it+github@posteo.com>
2026-06-24 10:53:53 +0200

    unterricht

--- Jobtastic/Controllers/HomeController.cs
+++ Jobtastic/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Jobtastic.Data;
 using Jobtastic.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,15 +8,18 @@ namespace Jobtastic.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly ApplicationDbContext _context;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
 		{
 			_logger = logger;
+			_context = context;
 		}
 
 		public IActionResult Index()
 		{
-			return View();
+			var allJobs = _context.JobPostings.ToList();
+			return View(allJobs);
 		}
 
 		public IActionResult Privacy()
