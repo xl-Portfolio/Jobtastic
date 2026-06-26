1690648a404036048646a9b10c5d0028b42f4165
Xenia <ixel+it+github@posteo.com>
2026-06-26 13:44:17 +0200

    alle identityuser, die nötig ware, ersetzt

--- Jobtastic/Controllers/JobPostingController.cs
+++ Jobtastic/Controllers/JobPostingController.cs
@@ -1,17 +1,70 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Jobtastic.Data;
+using Jobtastic.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Jobtastic.Controllers
 {
+    [Authorize]
+
     public class JobPostingController : Controller
     {
-        public IActionResult Index()
+
+        private readonly ApplicationDbContext _context;
+
+        public JobPostingController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
 
-        //public IActionResult Form()
-        //{
-        //    return Form 
-        //}
+        public IActionResult Index()
+        {
+            var AllJobs = _context.JobPostings.ToList();
+            return View(AllJobs);
+        }
+
+        public IActionResult Form(int iD) //Ergänzen um Ownerprüfung
+        {
+            if (iD != 0)
+            {
+                var particularJob = _context.JobPostings.SingleOrDefault(x => x.ID == iD);
+                return View(particularJob);
+            }
+            else return View();
+        }
+        public IActionResult CreateEditJob(JobPosting job, IFormFile file)
+        {
+            job.OwnerName = User.Identity.Name; //ownername muss noch implementiert werden im model
+            if (file != null)
+            {
+                using (var memoryStream = new MemoryStream()) //Bild als bytearray speichern in db
+                {
+                    file.CopyTo(memoryStream);
+                    var byteArray = memoryStream.ToArray();
+                    job.CompanyImage = byteArray; //muss in db angelegt werden (Logo?)
+                }
+            }
+            else { return NotFound(); }
+
+            if (job.ID == 0)
+            {
+                _context.JobPostings.Add(job);
+            }
+            else
+            {
+                var jobPostingbyID = _context.JobPostings.SingleOrDefault(x => x.ID == job.ID);
+                if (jobPostingbyID == null)
+                {
+                    return NotFound();
+                }
+                jobPostingbyID //.konkrete Property = job. konkrete Property
+                    //für jede Property, die sinn macht
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
