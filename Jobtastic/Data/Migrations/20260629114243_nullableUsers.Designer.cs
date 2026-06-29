d86867b6cdfde6d12f07258e61533a5f5805b2a9
Xenia <ixel+it+github@posteo.com>
2026-06-29 13:44:56 +0200

    registrierung/login gefixt

--- Jobtastic/Data/Migrations/20260629114243_nullableUsers.Designer.cs
+++ Jobtastic/Data/Migrations/20260629114243_nullableUsers.Designer.cs
@@ -4,6 +4,7 @@ using Jobtastic.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Jobtastic.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260629114243_nullableUsers")]
+    partial class nullableUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -169,11 +172,9 @@ namespace Jobtastic.Migrations
                         .HasColumnType("bit");
 
                     b.Property<string>("FirstName")
-                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("LastName")
-                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<bool>("LockoutEnabled")
--- Jobtastic/Models/User.cs
+++ Jobtastic/Models/User.cs
@@ -8,8 +8,8 @@ namespace Jobtastic.Models
     //}
     public class User : IdentityUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public string? ProfileImagePath { get; set; }
         public List<JobContact> Contacts { get; set; }
