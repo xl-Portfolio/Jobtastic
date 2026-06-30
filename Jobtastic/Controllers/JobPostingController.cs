92da3603f0a76309d2861c30265acbe39804b567
Xenia <ixel+it+github@posteo.com>
2026-06-30 15:30:14 +0200

    Dienstag3 datatables eingebunden

--- Jobtastic/Controllers/JobPostingController.cs
+++ Jobtastic/Controllers/JobPostingController.cs
@@ -31,49 +31,49 @@ namespace Jobtastic.Controllers
 
         public async Task<IActionResult> Form(int id) 
         {
+            if (id == 0)
+                return View();
             var particularJob = await _context.Postings.SingleOrDefaultAsync(x => x.ID == id);
             if (particularJob == null)
                 return NotFound();
-            if (particularJob.OwnerID != UserId)
+            if (particularJob.OwnerID != UserId && !User.IsInRole("Admin"))
                 return Unauthorized();
-            if (id == 0)
-                return View();
             return View(particularJob);
         }
-        public IActionResult CreateEditJob(JobPosting job, IFormFile file)
-        {
+        //public IActionResult CreateEditJob(JobPosting job, IFormFile file)
+        //{
 
-            Uploaddate und Expirydate mit IsOnline verknüpfen
-            job.OwnerName = User.Identity.Name; //ownername muss noch implementiert werden im model
-            if (file != null)
-            {
-                using (var memoryStream = new MemoryStream()) //Bild als bytearray speichern in db
-                {
-                    file.CopyTo(memoryStream);
-                    var byteArray = memoryStream.ToArray();
-                    job.CompanyImage = byteArray; //muss in db angelegt werden (Logo?)
-                }
-            }
-            else { return NotFound(); }
+        //    Uploaddate und Expirydate mit IsOnline verknüpfen
+        //    job.OwnerName = User.Identity.Name; //ownername muss noch implementiert werden im model
+        //    if (file != null)
+        //    {
+        //        using (var memoryStream = new MemoryStream()) //Bild als bytearray speichern in db
+        //        {
+        //            file.CopyTo(memoryStream);
+        //            var byteArray = memoryStream.ToArray();
+        //            job.CompanyImage = byteArray; //muss in db angelegt werden (Logo?)
+        //        }
+        //    }
+        //    else { return NotFound(); }
 
-            if (job.ID == 0)
-            {
-                _context.JobPostings.Add(job);
-            }
-            else
-            {
-                var jobPostingbyID = _context.JobPostings.SingleOrDefault(x => x.ID == job.ID);
-                if (jobPostingbyID == null)
-                {
-                    return NotFound();
-                }
-                jobPostingbyID //.konkrete Property = job. konkrete Property
-                    //für jede Property, die sinn macht
-                _context.SaveChanges();
-            }
+        //    if (job.ID == 0)
+        //    {
+        //        _context.JobPostings.Add(job);
+        //    }
+        //    else
+        //    {
+        //        var jobPostingbyID = _context.JobPostings.SingleOrDefault(x => x.ID == job.ID);
+        //        if (jobPostingbyID == null)
+        //        {
+        //            return NotFound();
+        //        }
+        //        jobPostingbyID //.konkrete Property = job. konkrete Property
+        //            //für jede Property, die sinn macht
+        //        _context.SaveChanges();
+        //    }
 
-            return RedirectToAction("Index");
-        }
+        //    return RedirectToAction("Index");
+        //}
 
 
     }
