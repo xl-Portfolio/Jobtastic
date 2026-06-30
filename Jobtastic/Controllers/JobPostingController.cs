68e1a56d8a192232d3a7c482080590eca0e6109d
Xenia <ixel+it+github@posteo.com>
2026-06-30 13:14:47 +0200

    Dienstag3 Anpassung JobPostingController läuft

--- Jobtastic/Controllers/JobPostingController.cs
+++ Jobtastic/Controllers/JobPostingController.cs
@@ -3,6 +3,7 @@ using Jobtastic.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace Jobtastic.Controllers
 {
@@ -12,6 +13,7 @@ namespace Jobtastic.Controllers
     {
 
         private readonly ApplicationDbContext _context;
+        private string? UserId => User.FindFirstValue(ClaimTypes.NameIdentifier);
 
         public JobPostingController(ApplicationDbContext context)
         {
@@ -21,54 +23,57 @@ namespace Jobtastic.Controllers
         public async Task<IActionResult> Index()
         {
             var AllJobs = await _context.Postings
-                .Include(j => j.Company) //Hier weiter
+                .Where(x => x.OwnerID == UserId)
+                .Include(j => j.Company)
                 .ToListAsync();
             return View(AllJobs);
         }
 
-        public async Task<IActionResult> Form(int iD) //Ergänzen um Ownerprüfung
+        public async Task<IActionResult> Form(int id) 
         {
-            if (iD != 0)
-            {
-                var particularJob = await _context.Postings.SingleOrDefaultAsync(x => x.ID == iD);
-                return View(particularJob);
-            }
-            else return View();
+            var particularJob = await _context.Postings.SingleOrDefaultAsync(x => x.ID == id);
+            if (particularJob == null)
+                return NotFound();
+            if (particularJob.OwnerID != UserId)
+                return Unauthorized();
+            if (id == 0)
+                return View();
+            return View(particularJob);
         }
-        //public IActionResult CreateEditJob(JobPosting job, IFormFile file)
-        //{
+        public IActionResult CreateEditJob(JobPosting job, IFormFile file)
+        {
 
-        // Uploaddate und Expirydate mit IsOnline verknüpfen
-        //    job.OwnerName = User.Identity.Name; //ownername muss noch implementiert werden im model
-        //    if (file != null)
-        //    {
-        //        using (var memoryStream = new MemoryStream()) //Bild als bytearray speichern in db
-        //        {
-        //            file.CopyTo(memoryStream);
-        //            var byteArray = memoryStream.ToArray();
-        //            job.CompanyImage = byteArray; //muss in db angelegt werden (Logo?)
-        //        }
-        //    }
-        //    else { return NotFound(); }
+            Uploaddate und Expirydate mit IsOnline verknüpfen
+            job.OwnerName = User.Identity.Name; //ownername muss noch implementiert werden im model
+            if (file != null)
+            {
+                using (var memoryStream = new MemoryStream()) //Bild als bytearray speichern in db
+                {
+                    file.CopyTo(memoryStream);
+                    var byteArray = memoryStream.ToArray();
+                    job.CompanyImage = byteArray; //muss in db angelegt werden (Logo?)
+                }
+            }
+            else { return NotFound(); }
 
-        //    if (job.ID == 0)
-        //    {
-        //        _context.JobPostings.Add(job);
-        //    }
-        //    else
-        //    {
-        //        var jobPostingbyID = _context.JobPostings.SingleOrDefault(x => x.ID == job.ID);
-        //        if (jobPostingbyID == null)
-        //        {
-        //            return NotFound();
-        //        }
-        //        jobPostingbyID //.konkrete Property = job. konkrete Property
-        //            //für jede Property, die sinn macht
-        //        _context.SaveChanges();
-        //    }
+            if (job.ID == 0)
+            {
+                _context.JobPostings.Add(job);
+            }
+            else
+            {
+                var jobPostingbyID = _context.JobPostings.SingleOrDefault(x => x.ID == job.ID);
+                if (jobPostingbyID == null)
+                {
+                    return NotFound();
+                }
+                jobPostingbyID //.konkrete Property = job. konkrete Property
+                    //für jede Property, die sinn macht
+                _context.SaveChanges();
+            }
 
-        //    return RedirectToAction("Index");
-        //}
+            return RedirectToAction("Index");
+        }
 
 
     }
