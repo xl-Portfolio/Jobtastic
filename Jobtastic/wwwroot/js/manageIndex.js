42bac05d2c41427f024e2db5d6cbc296c25946e6
Xenia <ixel+it+github@posteo.com>
2026-07-15 13:07:23 +0200

    js für manage index steht. Passwort ändern-button funktioniert nicht

--- Jobtastic/wwwroot/js/manageIndex.js
+++ Jobtastic/wwwroot/js/manageIndex.js
@@ -1,1 +1,14 @@
-﻿
+﻿var displayData = document.getElementById("displayData");
+var editDataBtn = document.getElementById("editDataBtn");
+var editData = document.getElementById("editData");
+editDataBtn.addEventListener("click", function () {
+    displayData.classList.add("d-none");
+    editData.classList.remove("d-none");
+});
+
+var editPasswordBtn = document.getElementById("editPasswordBtn");
+var editPassword = document.getElementById("editPassword");
+editPasswordBtn.addEventListener("click", function () {
+    editPassword.classList.remove("d-none");
+    editPasswordBtn.classList.add("d-none");
+});
