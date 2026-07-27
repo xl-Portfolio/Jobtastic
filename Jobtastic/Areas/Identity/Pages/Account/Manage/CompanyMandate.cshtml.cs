5e4cd1504e649189825d41cd6517dd30772754e0
Xenia <ixel+it+github@posteo.com>
2026-07-27 13:57:30 +0200

    montag feierabend

--- Jobtastic/Areas/Identity/Pages/Account/Manage/CompanyMandate.cshtml.cs
+++ Jobtastic/Areas/Identity/Pages/Account/Manage/CompanyMandate.cshtml.cs
@@ -3,6 +3,8 @@ using Jobtastic.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Jobtastic.Areas.Identity.Pages.Account.Manage
 {
@@ -13,6 +15,11 @@ namespace Jobtastic.Areas.Identity.Pages.Account.Manage
 
         [BindProperty]
         public InputModel Input { get; set; }
+        public List<Company> Companies { get; private set; }
+
+        [BindProperty]
+        public int CompanyId { get; set; }
+
 
         public CompanyMandateModel(UserManager<User> userManager, ApplicationDbContext context)
         {
@@ -21,16 +28,33 @@ namespace Jobtastic.Areas.Identity.Pages.Account.Manage
         }
         public class InputModel
         {
-            
+            [Required]
             public string Name { get; set; }
             public string? Description { get; set; }
             public string? LogoImageSource { get; set; }
             public string? WebsiteURL { get; set; }
             public int? NumberEmployees { get; set; }
         }
+        private async Task<User> GetCompaniesByUserAsync()
+        {
+            var userId = _userManager.GetUserId(User);
+            var user = await _context.Users
+                .Include(u => u.Companies)
+                .FirstOrDefaultAsync(u => u.Id == userId);
+            return user;
+        }
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await GetCompaniesByUserAsync();
+            if (user == null)
+                return NotFound();
 
-        //public void OnGet()
-        //{
-        //}
+            Companies = user.Companies.ToList();
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAddMandateAsync()
+        {
+            return Page();
+        }
     }
 }
--- Jobtastic/wwwroot/js/manageMandates.js
+++ Jobtastic/wwwroot/js/manageMandates.js
@@ -1,1 +1,6 @@
-﻿
+﻿var createMandateBtn = document.getElementById("createMandateBtn");
+var addMandate = document.getElementById("addMandate");
+createMandateBtn.addEventListener("click", function () {
+    addMandate.classList.remove("d-none");
+    createMandateBtn.classList.add("d-none");
+});
