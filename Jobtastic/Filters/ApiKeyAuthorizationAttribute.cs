7b0bfae41a60304c75d85d6bacf8f131aec11ba0
xenia.landeis@outlook.de <xenia.landeis@outlook.de>
2026-08-20 13:45:15 +0200

    Form (JobPosting) angepasst: ViewBag für Companies funktioniert & css wurde angepasst

---
+++ Jobtastic/Filters/ApiKeyAuthorizationAttribute.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Jobtastic.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)] //Wie kann man Attributes Parameter übergeben?
+    public class ApiKeyAuthorizationAttribute : Attribute, IAsyncActionFilter
+    {
+        //private readonly string? _requiredRole;
+        //public ApiKeyAuthorizationAttribute(string requiredRole = null)
+        //{
+        //    _requiredRole = requiredRole;
+        //}
+
+        /// <summary>
+        /// checks API Key in the request header and returns Unauthorized if not present or invalid
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="next"></param>
+        /// <returns></returns>
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue("ApiKey", out var extractedApiKey))
+            {
+                context.Result = new Microsoft.AspNetCore.Mvc.UnauthorizedResult();
+                return;
+            }
+            else if (extractedApiKey == "12345" /*|| _requiredRole != "Admin"*/)
+            {
+                context.Result = new Microsoft.AspNetCore.Mvc.UnauthorizedResult();
+                return;
+            }
+            else
+            {
+                var config = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+                var key = config.GetValue<string>("ApiKey");
+                if (string.IsNullOrEmpty(key) || extractedApiKey != key)
+                {
+                    context.Result = new Microsoft.AspNetCore.Mvc.UnauthorizedResult();
+                    return;
+                }
+            }
+            await next();
+        }
+    }
+}
