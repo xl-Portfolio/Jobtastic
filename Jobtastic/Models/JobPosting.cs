6d6bb563fefedfa911f091e3e198ee35bb5f493c
Xenia <ixel+it+github@posteo.com>
2026-08-24 11:33:56 +0200

    Mandate entfernen eingefügt (bug besteht)

--- Jobtastic/Models/JobPosting.cs
+++ Jobtastic/Models/JobPosting.cs
@@ -9,7 +9,9 @@ namespace Jobtastic.Models
         public int CompanyID { get; set; } //(FK)
         public Company Company { get; set; }
         public string JobTitle { get; set; }
+        [MaxLength(160)]
         public string Header { get; set; }
+        [MaxLength(5000)]
         public string JobDescription { get; set; }
         public string JobLocation { get; set; } 
         public double AnnualSalary { get; set; }
