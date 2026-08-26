e1ab6164a23be9b0d16bb46d2ffd8ca60e4e82eb
Xenia <ixel+it+github@posteo.com>
2026-08-26 15:33:27 +0200

    AccountAccess testbar gemacht und Tests hinzugefügt

---
+++ Jobtastic/Authorization/AccountAccess.cs
@@ -0,0 +1,8 @@
+﻿namespace Jobtastic.Authorization
+{
+    public static class AccountAccess
+    {
+        public static bool MayTarget(string? callerId, string? requestedUserId, bool isAdmin) =>
+            string.IsNullOrEmpty(requestedUserId) || requestedUserId == callerId || isAdmin;
+    }
+}
